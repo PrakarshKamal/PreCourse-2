51ffc4e0deb812b1baa1dcbb658727d07959e8ed
PrakarshKamal <kamalp@mcmaster.ca>
2026-02-03 16:12:41 -0500

    Complete Precourse-2

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,3 +1,6 @@
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
 class LinkedList 
 { 
     Node head; // head of linked list 
@@ -18,8 +21,18 @@ class LinkedList
    //Complete this function
     void printMiddle() 
     { 
-        //Write your code here
-	//Implement using Fast and slow pointers
+        if (head == null) {
+            System.out.println("The list is empty");
+            return;
+        }
+
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        System.out.println("The middle element is " + slow.data);
     } 
   
     public void push(int new_data) 
--- Exercise_4.java
+++ Exercise_4.java
@@ -1,19 +1,60 @@
+// Time Complexity: O(n log n)
+// Space Complexity: O(n)
+
 class MergeSort 
 { 
     // Merges two subarrays of arr[]. 
     // First subarray is arr[l..m] 
     // Second subarray is arr[m+1..r] 
     void merge(int arr[], int l, int m, int r) 
-    {  
-       //Your code here  
+    {    
+       int len1 = m - l + 1;
+       int len2 = r - m;
+       int[] left = new int[len1];
+       int[] right = new int[len2];
+
+       for (int i = 0; i < len1; i++) {
+        left[i] = arr[l+i];
+       }
+       for (int j = 0; j < len2; j++) {
+        right[j] = arr[m+1+j];
+       }
+       int i = 0; // index of left array
+       int j = 0; // index of right array
+       int k = l; // index of merged array
+
+       while(i < len1 && j < len2) {
+            if (left[i] <= right[j]) {
+                arr[k] = left[i];
+                i++;
+            } else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < len1) {
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+        while (j < len2) {
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
     } 
   
     // Main function that sorts arr[l..r] using 
     // merge() 
     void sort(int arr[], int l, int r) 
     { 
-	//Write your code here
-        //Call mergeSort from here 
+        if (l < r) {
+            int mid = (l + r) / 2;
+            sort(arr, l, mid);
+            sort(arr, mid+1, r);
+            merge(arr, l, mid, r);
+        }
     } 
   
     /* A utility function to print array of size n */
